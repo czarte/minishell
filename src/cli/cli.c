ddbb94ba51898aaef1d8d59493b4660dd3041961
Stepan Melichar <smelicha@c3r6s3.42prague.com>
2024-07-17 18:22:32 +0200

    bugfix of scan_folders function, when the first folder does not exist it will not continue in scanning for binaries so there are no executables in the list to be executed, now it skips the emtpy folder until all folders have been gone through

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -75,15 +75,12 @@ int	cli(t_data *data)
 
 	rl_catch_signals = 0;
 	rl_change_environment = 0;
-	// signal(SIGINT, signal_handler);
 	struct sigaction sa;
     sa.sa_handler = signal_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
     sigaction(SIGINT, &sa, NULL);
 	sigaction(SIGQUIT, &sa, NULL);
-
-
 	while (1)
 	{
 		create_prompt(data);
