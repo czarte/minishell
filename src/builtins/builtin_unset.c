e198a88b7f1d1f9dd9ca410d5c28b02c6d9ea920
Stepan Melichar <smelicha@c2r6s7.42prague.com>
2024-07-21 17:18:43 +0200

    unset working, seems there are no leaks, might need further testing, now I will work on norminettization

--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -6,28 +6,34 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 18:19:31 by smelicha          #+#    #+#             */
-/*   Updated: 2024/07/21 16:24:45 by smelicha         ###   ########.fr       */
+/*   Updated: 2024/07/21 17:16:25 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
-int	copy_remove_envp(char **new_envp, char **envp, int var_pos)
+int	copy_remove_envp(char **new_envp, char **envp, int var_pos, int n)
 {
 	int	i;
+	int	j;
 
 	i = 0;
-	while (envp && envp[i])
+	j = 0;
+	n++;
+	if (new_envp == NULL || var_pos < 0)
+		return (0);
+	while (j < n)
 	{
-		new_envp[i] = ft_memcpy(envp[i]);
+		if (j == var_pos)
+			j++;
+		new_envp[i] = ft_memcpy(envp[j]);
 		if (new_envp[i] == NULL)
 		{
 			perror("Copying envp");
 			return (-1);
 		}
 		i++;
-		if (i == var_pos)
-			i++;
+		j++;
 	}
 	return (0);
 }
@@ -48,11 +54,15 @@ char	**allocate_new_envp(int	n)
 {
 	char	**new_envp;
 
+	printf("Number from allocate new envp: %i\n", n);
 	new_envp = NULL;
-	new_envp = malloc(sizeof(char*) * n);
-	if (!new_envp)
-		return (NULL);
-	double_pointer_null_init(new_envp, n);
+	if (n > 1)
+	{
+		new_envp = malloc(sizeof(char*) * n);
+		if (!new_envp)
+			return (NULL);
+		double_pointer_null_init(new_envp, n);
+	}
 	return (new_envp);
 }
 
@@ -78,13 +88,15 @@ int	unset(t_token_chain *current, t_data *data)
 		if (var_pos >= 0)
 			temp_flag = 1;
 	}
+	if (var_pos < 0)
+		return (0);
 	printf("temp_flag: %i, var_pos: %i var_name: %s\n", temp_flag, var_pos,
 		current->next->token);
 	if (temp_flag == 1)
 	{
 		printf("variable from pos: %s\n", data->local_temp_envp[var_pos]);
 		new_envp = allocate_new_envp(num_of_vars(data->local_temp_envp));
-		copy_remove_envp(new_envp, data->local_temp_envp, var_pos);
+		copy_remove_envp(new_envp, data->local_temp_envp, var_pos, (num_of_vars(data->envp) - 1));
 		free_old_envp(data->local_temp_envp);
 		data->local_temp_envp = new_envp;
 	}
@@ -92,7 +104,7 @@ int	unset(t_token_chain *current, t_data *data)
 	{
 		printf("variable from pos: %s\n", data->envp[var_pos]);
 		new_envp = allocate_new_envp(num_of_vars(data->envp));
-		copy_remove_envp(new_envp, data->envp, var_pos);
+		copy_remove_envp(new_envp, data->envp, var_pos, (num_of_vars(data->envp) - 1));
 		free_old_envp(data->envp);
 		data->envp = new_envp;
 	}
