ab4b18201772be03bf333c08fe9904c6ced68d5f
Stepan Melichar <smelicha@c2r6s7.42prague.com>
2024-07-21 17:30:42 +0200

    unset now according to norm

--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 18:19:31 by smelicha          #+#    #+#             */
-/*   Updated: 2024/07/21 17:16:25 by smelicha         ###   ########.fr       */
+/*   Updated: 2024/07/21 17:29:06 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ void	double_pointer_null_init(char **arr, int n)
 	}
 }
 
-char	**allocate_new_envp(int	n)
+char	**allocate_new_envp(int n)
 {
 	char	**new_envp;
 
@@ -58,12 +58,33 @@ char	**allocate_new_envp(int	n)
 	new_envp = NULL;
 	if (n > 1)
 	{
-		new_envp = malloc(sizeof(char*) * n);
+		new_envp = malloc(sizeof(char *) * n);
 		if (!new_envp)
 			return (NULL);
 		double_pointer_null_init(new_envp, n);
 	}
 	return (new_envp);
+}
+
+int	finish_unset(int var_pos, char **new_envp, char temp_flag, t_data *data)
+{
+	if (temp_flag == 1)
+	{
+		new_envp = allocate_new_envp(num_of_vars(data->local_temp_envp));
+		copy_remove_envp(new_envp, data->local_temp_envp, var_pos,
+			(num_of_vars(data->envp) - 1));
+		free_old_envp(data->local_temp_envp);
+		data->local_temp_envp = new_envp;
+	}
+	else if (temp_flag == 0)
+	{
+		new_envp = allocate_new_envp(num_of_vars(data->envp));
+		copy_remove_envp(new_envp, data->envp, var_pos,
+			(num_of_vars(data->envp) - 1));
+		free_old_envp(data->envp);
+		data->envp = new_envp;
+	}
+	return (0);
 }
 
 //TODO Deleting the entry in envp or local temp envp from the var pos
@@ -77,40 +98,16 @@ int	unset(t_token_chain *current, t_data *data)
 	if (!current->next)
 		return (0);
 	var_pos = check_envp_for_duplicate(data->envp, current->next->token);
-	printf("var_pos from envp: %i\n", var_pos);
 	if (var_pos >= 0)
 		temp_flag = 0;
 	if (var_pos < 0)
 	{
 		var_pos = check_envp_for_duplicate(data->local_temp_envp,
 				current->next->token);
-		printf("var_pos from temp: %i\n", var_pos);
 		if (var_pos >= 0)
 			temp_flag = 1;
 	}
 	if (var_pos < 0)
 		return (0);
-	printf("temp_flag: %i, var_pos: %i var_name: %s\n", temp_flag, var_pos,
-		current->next->token);
-	if (temp_flag == 1)
-	{
-		printf("variable from pos: %s\n", data->local_temp_envp[var_pos]);
-		new_envp = allocate_new_envp(num_of_vars(data->local_temp_envp));
-		copy_remove_envp(new_envp, data->local_temp_envp, var_pos, (num_of_vars(data->envp) - 1));
-		free_old_envp(data->local_temp_envp);
-		data->local_temp_envp = new_envp;
-	}
-	else if (temp_flag == 0)
-	{
-		printf("variable from pos: %s\n", data->envp[var_pos]);
-		new_envp = allocate_new_envp(num_of_vars(data->envp));
-		copy_remove_envp(new_envp, data->envp, var_pos, (num_of_vars(data->envp) - 1));
-		free_old_envp(data->envp);
-		data->envp = new_envp;
-	}
-	else
-	{
-		printf("Nothing to unset :(\n");
-	}
-	return (0);
+	return (finish_unset(var_pos, new_envp, temp_flag, data));
 }
